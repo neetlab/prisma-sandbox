812cdcfe060584c0c7e9790f9ab2c0f8d65095bd
Ryo Igarashi <n33t5hin@gmail.com>
2020-12-19 15:13:37 +0900

    Add a query

--- script.ts
+++ script.ts
@@ -4,7 +4,33 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-  // ... you will write your Prisma Client queries here
+
+  /*
+  create  
+  const post = await prisma.post.create({
+    where: { id: 2 },
+    data: {
+      title: 'Prisma makes databases easy',
+      // "Nested write"
+      author: {
+        connect: { email: 'sarah@prisma.io' },
+      },
+    },
+  });
+  */
+
+  const post = await prisma.post.update({
+    where: { id: 2 },
+    data: { published: true },
+  });
+  console.log(post);
+
+  const allUsers = await prisma.user.findMany({
+    // JOIN relation
+    include: { posts: true },
+  });
+
+  console.log(allUsers);
 }
 
 main()
